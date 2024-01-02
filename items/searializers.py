155b6e59da6720bae90d6c7a5d4c49ba89c4e9e2
aasmeen <aasmeen1700@gmail.com>
2024-01-03 03:09:40 +0530

    create CRUD APIs for items

---
+++ items/searializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from items.models import Items
+
+
+class ItemsSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ('user', 'name', 'price', 'selling_price')
+        model = Items
+    
+    def get_user(self, instance):
+        return instance.user.username
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class UpdateItemSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(required=False)
+    price = serializers.IntegerField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        user = self.context['request'].user
+        if user and user.is_content_editor:
+            self.fields.pop('price')
+
+    class Meta:
+        fields = ('name', 'price')
+        model = Items
--- items/views.py
+++ items/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveAPIView, ListAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from items.constants import CONTENT_WRITTER_UPDATE_FIELDS
+from items.models import Items
+from items.permissions import CanCreateorDeleteItem, CanUpdateItem
+from items.searializers import ItemsSerializer, UpdateItemSerializer
+
+
+class CreateItemAPIView(CreateAPIView):
+    permission_classes = [IsAuthenticated, CanCreateorDeleteItem]
+    serializer_class = ItemsSerializer
+
+
+class GetItemAPIView(RetrieveAPIView):
+    queryset = Items.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = ItemsSerializer
+
+
+class ListItemsAPIView(ListAPIView):
+    queryset = Items.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = ItemsSerializer
+
+
+class UpdateItemAPIView(UpdateAPIView):
+    method_allowed = ['patch']
+    queryset = Items.objects.all()
+    permission_classes = [IsAuthenticated, CanUpdateItem]
+    serializer_class = UpdateItemSerializer
+
+    def update(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_content_editor and set(request.data.keys()) - CONTENT_WRITTER_UPDATE_FIELDS:
+            raise ValidationError("Content Editors can only update the 'name' field.")
+        return super().update(request, *args, **kwargs)
+
+
+class DeleteItemAPIView(DestroyAPIView):
+    queryset = Items.objects.all()
+    permission_classes = [IsAuthenticated, CanCreateorDeleteItem]
+    serializer_class = ItemsSerializer
